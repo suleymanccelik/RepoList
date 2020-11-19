89423ae0a2d35023eeab8e51ba8bd4a1049f34f7
Süleyman Çelik <suleymancelik1989@gmail.com>
2020-11-19 22:58:41 +0300

    project initial commit

---
+++ app/src/main/java/com/suleymancelik/github/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.suleymancelik.github
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
